aa7e713c8a2cde136839bd730e98f9eafdda85a7
mazongguang <mazongguang@jd.com>
2017-12-15 18:47:53 +0800

    mybatis ready

--- src/main/resources/com/Jcloud/SqlParser/Dao/HiveTestModelMapper.java
+++ src/main/resources/com/Jcloud/SqlParser/Dao/HiveTestModelMapper.java
@@ -4,7 +4,9 @@ import com.Jcloud.SqlParser.Model.SqlModel.HiveTestModel;
 import com.Jcloud.SqlParser.Model.SqlModel.HiveTestModelExample;
 import java.util.List;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface HiveTestModelMapper {
     long countByExample(HiveTestModelExample example);
 
--- src/main/resources/com/Jcloud/SqlParser/Dao/MysqlTestModelMapper.java
+++ src/main/resources/com/Jcloud/SqlParser/Dao/MysqlTestModelMapper.java
@@ -4,7 +4,9 @@ import com.Jcloud.SqlParser.Model.SqlModel.MysqlTestModel;
 import com.Jcloud.SqlParser.Model.SqlModel.MysqlTestModelExample;
 import java.util.List;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MysqlTestModelMapper {
     long countByExample(MysqlTestModelExample example);
 
